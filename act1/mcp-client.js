efdc034a6e54abd0f64724af956b935b1191fe7c
Nicvini0160 <nicolasoliver0160@gmail.com>
2025-03-28 23:33:44 -0300

    Update mcp-client.js

--- act1/mcp-client.js
+++ act1/mcp-client.js
@@ -1,14 +1,20 @@
 import {Client} from '@modelcontextprotocol/sdk/client/index.js'
 import {StdioClientTransport} from '@modelcontextprotocol/sdk/client/stdio.js'
+const client= new client({ 
+  name:nicolas, version nicolas 
+version string like ´´1.0.0´´
 
-// TODO: Create a client instance with name "disrupt-client" and version "0.1.0"
 
 // TODO: Create a stdio transport that connects to "./act1/mcp-server.js"
-
+const transport=  new StdioClientTransport
+command: ´´node´´
+args: [´./server.Js];
+Path to the server files});
 async function main() {
   try {
     // TODO: Connect the client to the server using the transport
-
+connection syntax
+    await client.connect(transport);
     console.log('Connected to server successfully!')
   } catch (error) {
     console.error('Failed to connect:', error)
